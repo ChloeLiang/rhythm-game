35d7fdf4598b981237fb87c1f61b2c23583c7ab9
ChloeLiang <nliangxin@gmail.com>
2018-09-14 22:17:25 +0800

    Add game style

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 window.onload = function () {
-  var canvas = document.getElementById('canvas');
-  var ctx;
-  var keyHeight = canvas.height / 7;
-  var keyWidth = canvas.width / 8;
-
   document.onkeydown = function (event) {
     event = event || window.event;
     if (event.keyCode === 65) {
@@ -25,28 +20,4 @@ window.onload = function () {
     }
   };
 
-  var start = function () {
-    if (canvas.getContext) {
-      ctx = canvas.getContext('2d');
-      drawKeyPads(ctx);
-
-      console.log(canvas.width, canvas.height);
-    } else {
-      alert('Canvas is not supported!');
-    }
-  };
-
-  var drawKeyPads = function (ctx) {
-    var i;
-    for (i = 0; i < 8; i++) {
-      if (i % 2 === 0) {
-        ctx.fillStyle = 'red';
-      } else {
-        ctx.fillStyle = 'blue';
-      }
-      ctx.fillRect(keyWidth * i, canvas.height - keyHeight, keyWidth, keyHeight);
-    }
-  };
-
-  start();
 }
