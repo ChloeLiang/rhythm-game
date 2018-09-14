0f517ca84f47bf282c08b6b94c634f80b5ec9a28
ChloeLiang <nliangxin@gmail.com>
2018-09-14 21:08:40 +0800

    Add key press event

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ window.onload = function () {
   var keyHeight = canvas.height / 7;
   var keyWidth = canvas.width / 8;
 
+  document.onkeydown = function (event) {
+    event = event || window.event;
+    if (event.keyCode === 65) {
+      console.log('A pressed');
+    } else if (event.keyCode === 83) {
+      console.log('S pressed');
+    } else if (event.keyCode === 68) {
+      console.log('D pressed');
+    } else if (event.keyCode === 70) {
+      console.log('F pressed');
+    } else if (event.keyCode === 74) {
+      console.log('J pressed');
+    } else if (event.keyCode === 75) {
+      console.log('K pressed');
+    } else if (event.keyCode === 76) {
+      console.log('L pressed');
+    } else if (event.keyCode === 186) {
+      console.log('; pressed');
+    }
+  };
+
   var start = function () {
     if (canvas.getContext) {
       ctx = canvas.getContext('2d');
